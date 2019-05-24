bac2aecdf0a61148a7189df39a5fd42cb99c12c2
lambert <dyzxlingyang@163.com>
2019-05-24 16:02:25 +0800

    feat: add __REDUX_DEVTOOLS_EXTENSION__

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,14 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import reducer from './reducers';
 
-// 导出函数，以便客户端和服务端根据初始state创建store
 export default (store) => {
   return createStore(
     reducer,
     store,
-    applyMiddleware(thunkMiddleware) // 允许store能dispatch函数
+    compose(
+      applyMiddleware(thunkMiddleware),
+      (process.env.NODE_ENV !== 'server' && window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()) || compose
+    )
   );
 };
